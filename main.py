07b2fd32655bb395630210b6e84e2656db0cc666
MrHishprung <mail.mrhishprung@gmail.com>
2021-06-16 20:34:38 +0200

    Expanded the program with classes and 'File not found' exception

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import pandas as pd
-plik = input("Podaj nazwe pliku źródłowego (bez rozszerzenia): ")
-nazwaxlsx = "D:\\" + plik + ".xlsx"
-nazwatxt =  "D:\\" + plik + ".txt"
-df = pd.read_excel(nazwaxlsx, sheet_name='Sheet1', index_col=0)
-with open(nazwatxt, 'w') as pliktxt:
-    df.to_string(pliktxt, index=True)
+
+class Program:
+    def __init__(self):
+        file = input("Insert file name (without extension): ")
+        self.namexlsx = "D:\\" + file + ".xlsx"
+        self.nametxt = "D:\\" + file + ".txt"
+        Program.export(self.namexlsx, self.namexlsx)
+
+    def export(namexlsx, nametxt):
+        try:
+            df = pd.read_excel(namexlsx, sheet_name='Sheet1', index_col=0)
+            with open(nametxt, 'w') as filetxt:
+                df.to_string(filetxt, index=True)
+            print("Conversion to .txt file has been successful.")
+
+        except FileNotFoundError:
+            print("File not found, check file name again.")
+            print("Conversion to .txt file has failed.")
+
+
+Program()
