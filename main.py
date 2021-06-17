7a13b64c48cbb69681f3d03bbee300377fec3b50
MrHishprung <mail.mrhishprung@gmail.com>
2021-06-17 19:30:07 +0200

    Changed the code to convert into .csv file.

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import pandas as pd
+
+
 # If pandas is not installed: pip install pandas
 
 class Program:
     def __init__(self):
-        file = input("Insert file name (without extension): ")
-        self.namexlsx = "D:\\" + file + ".xlsx"
-        self.nametxt = "D:\\" + file + ".txt"
-        Program.export(self.namexlsx, self.namexlsx)
+         file = input("Insert file name (without extension): ")
+        self.namexlsx = "D:\\xlsx\\" + file + ".xlsx"
+        self.namecsv = "D:\\csv\\" + file + ".csv"
+        Program.export(self.namexlsx, self.namecsv)
 
-    def export(namexlsx, nametxt):
+    def export(namexlsx, namecsv):
         try:
-            df = pd.read_excel(namexlsx, sheet_name='Sheet1', index_col=0)
-            with open(nametxt, 'w') as filetxt:
-                df.to_string(filetxt, index=True)
-            print("Conversion to .txt file has been successful.")
+            read_file = pd.read_excel(namexlsx, sheet_name='Sheet1', index_col=0)
+            read_file.to_csv(namecsv, index=True, sep=',')
+            print("Conversion to .csv file has been successful.")
 
         except FileNotFoundError:
             print("File not found, check file name again.")
-            print("Conversion to .txt file has failed.")
+            print("Conversion to .csv file has failed.")
 
 
 Program()
